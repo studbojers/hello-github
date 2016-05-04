d49b796eb977d04bfc43b5ba0a067716f257bfda
studbojers <sdebastian.bojers@stud.pointcollege.fi>
2016-05-04 19:59:05 +0300

    Luotu ensimmäinen GitHub-projekti hello-github

---
+++ hello-github/hello-github/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub!");
+            Console.WriteLine("");
+            Console.WriteLine("Hit <enter> to quit...");
+            Console.ReadLine();
+        }
+    }
+}
